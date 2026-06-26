fd0b5cb9395c8f74877046f789a3867e37f9154a
Swaraj Fugare <swarajfugare@Swarajs-MacBook-Air.local>
2026-06-26 18:48:02 +0530

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,21 @@
+import type { Route } from "./+types/home";
+import Navbar from "../../components/Navbar";
+
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <h1>Welcome to the Home Page</h1>
+    </div>
+
+
+  )
+}
